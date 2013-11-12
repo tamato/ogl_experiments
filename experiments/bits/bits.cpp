2d2e4e1010e0fd48552a8913e169d5f7ddc04477
terence amato <terence.amato@gmail.com>
2013-11-11 21:48:16 -0700

    updates

--- experiments/bits/bits.cpp
+++ experiments/bits/bits.cpp
@@ -10,45 +10,41 @@
 
 using namespace std;
 
-void pairwise_sum(unsigned int c, unsigned int r, const string& test_name ){
+void pair_sidewise_sum(unsigned int c, unsigned int r, const string& test_name ){
     static const unsigned int odd_mask  = 0xAAAAAAAA; // odd mask  = 1010 1010 ...
     static const unsigned int even_mask = 0x55555555; // even mask = 0101 0101 ...
     unsigned int d = (c & even_mask) + ((c & odd_mask)>>1); // from the paper mentioned above in section 6.2
     cout << test_name << " " << boolalpha << (r == d) << endl;
 }
 
-void pairwise_sum_sep(unsigned int a, unsigned int b, unsigned int r0, unsigned int r1, const string& test_name ){
+void parallel_sum(unsigned int x0, unsigned int x1, unsigned int r0, unsigned int r1, const string& test_name ){
     // the values coming in will have counters between [0-2] contained in 2 bits
     // the sumed vaules would be stored in 4 bits
     // if  a : 0010 0010 0001 0001
     // and b : 0001 0010 0010 0001 and then the sum of these two would be
     //   sum : 0011 0100 0011 0010
-    static const unsigned int mask_33 = 0x33333333;   // 0011 0011 ...
-    static const unsigned int mask_CC = 0xCCCCCCCC;   // 1100 1100 ...
-    static const unsigned int odd_mask  = 0xAAAAAAAA; // odd mask  = 1010 1010 ...
-    static const unsigned int even_mask = 0x55555555; // even mask = 0101 0101 ...
-    unsigned int x0 = a;
-    unsigned int x1 = b;
+    // The values are stored into two variables "even" and "odd"
+    // They hold the sums of the bit pair they relate to.
+    static const unsigned int even_mask = 0x33333333;   // 0011 0011 ...
+    static const unsigned int odd_mask  = 0xCCCCCCCC;   // 1100 1100 ...
     unsigned int even = 0;
     unsigned int odd = 0;
 
     // sum the lower bits of each nibble
-    even = (x0 & mask_33) + (x1 & mask_33);
+    even = (x0 & even_mask) + (x1 & even_mask);
     // sum the upper bits of each nibble
-    odd  = ((x0 & mask_CC) >> 2) + ((x1 & mask_CC) >> 2);
+    odd  = ((x0 & odd_mask) >> 2) + ((x1 & odd_mask) >> 2);
 
-    std::bitset<32> pa(a);
-    std::bitset<32> pb(b);
-    std::bitset<32> e(even);
-    std::bitset<32> o(odd);
-    cout << a;
-    cout << test_name << boolalpha
-         << "\n" << "a: " << pa
-         << "\n" << "b: " << pb
-         << "\n" << "e: " << e
-         << "\n" << "o: " << o
-         << "\n" << (r0 == even) << " " << (r1 == odd) 
-         << endl;
+    std::bitset<8> pa(x0);
+    std::bitset<8> pb(x1);
+    std::bitset<8> e(even);
+    std::bitset<8> o(odd);
+    cout << test_name << boolalpha << " " << (r0 == even) << " " << (r1 == odd) << endl;
+         // << "\n\t" << "a: " << pa << "    " << pa
+         // << "\n\t" << "b: " << pb << "    " << pb
+         // << "\n\t" << "e: " << e  << " o: " << o
+         // << "\n\t" << (r0 == even) << " " << (r1 == odd)
+         // << endl;
 }
 
 int main()
@@ -61,54 +57,93 @@ int main()
     {
         c = 0x03;   // 0000 0011
         ans = 0x02; // 0000 0010
-        pairwise_sum(c, ans, "Test 1");
+        pair_sidewise_sum(c, ans, "Test 1");
     }
 
     {
         c = 0x0F;   // 0000 1111
         ans = 0x0A; // 0000 1010
-        pairwise_sum(c, ans, "Test 2");
+        pair_sidewise_sum(c, ans, "Test 2");
     }
 
     {
         c = 0xA5;   // 1010 0101
         ans = 0x55; // 0101 0101
-        pairwise_sum(c, ans, "Test 3");
+        pair_sidewise_sum(c, ans, "Test 3");
     }
 
     {
         c = 0x50;   // 0101 0000
         ans = 0x50; // 0101 0000
-        pairwise_sum(c, ans, "Test 4");
+        pair_sidewise_sum(c, ans, "Test 4");
     }
 
     {
         c = 0xBE;   // 1011 1110
         ans = 0x69; // 0110 1001
-        pairwise_sum(c, ans, "Test 5");
+        pair_sidewise_sum(c, ans, "Test 5");
     }
 
     {
         c = 0xFE;   // 1111 1110
         ans = 0xA9; // 1010 1001
-        pairwise_sum(c, ans, "Test 6");
+        pair_sidewise_sum(c, ans, "Test 6");
     }
 
     {   // combining tests 1,2,3,6
         c = 0x030FA5FE;
         ans = 0x020A55A9;
-        pairwise_sum(c, ans, "Test 7");
+        pair_sidewise_sum(c, ans, "Test 7");
     }
 
+    {
+        unsigned int a = 0xA5;  // 1010 0101
+        unsigned int b = 0x66;  // 0110 0110
 
-    {   // combining tests 1,2,3,6
-        unsigned int a = 0;  // 1010 0101
-        unsigned int b = 0;  // 0110 0110
-        
-        unsigned int r0 = 0; // 0100 0011
-        unsigned int r1 = 0; // 0011 0010 (its shifted by >> 2) 
-        
-        pairwise_sum_sep(a, b, r0, r1, "Test 10");
+        unsigned int r0 = 0x43; // 0100 0011
+        unsigned int r1 = 0x32; // 0011 0010 (its shifted by >> 2)
+
+        parallel_sum(a, b, r0, r1, "Test 10");
+    }
+
+    {
+        unsigned int a = 0x95;  // 1001 0101
+        unsigned int b = 0xA6;  // 1010 0110
+
+        unsigned int r0 = 0x33; // 0011 0011
+        unsigned int r1 = 0x42; // 0100 0010 (its shifted by >> 2)
+
+        parallel_sum(a, b, r0, r1, "Test 11");
+    }
+
+    {
+        unsigned int a = 0xAA;  // 1010 1010
+        unsigned int b = 0xAA;  // 1010 1010
+
+        unsigned int r0 = 0x44; // 0100 0100
+        unsigned int r1 = 0x44; // 0100 0100 (its shifted by >> 2)
+
+        parallel_sum(a, b, r0, r1, "Test 12");
+    }
+
+    {
+        unsigned int a = 0x59;  // 0101 1001
+        unsigned int b = 0x59;  // 0101 1001
+
+        unsigned int r0 = 0x22; // 0010 0010
+        unsigned int r1 = 0x24; // 0010 0100 (its shifted by >> 2)
+
+        parallel_sum(a, b, r0, r1, "Test 13");
+    }
+
+    {   // combining tests 10 - 13
+        unsigned int a = 0xA595AA59;  // 0101 1001
+        unsigned int b = 0x66A6AA59;  // 0101 1001
+
+        unsigned int r0 = 0x43334422; // 0010 0010
+        unsigned int r1 = 0x32424424; // 0010 0100 (its shifted by >> 2)
+
+        parallel_sum(a, b, r0, r1, "Test 14");
     }
     return 0;
 }
